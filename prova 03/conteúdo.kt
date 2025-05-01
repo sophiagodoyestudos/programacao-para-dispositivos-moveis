6cfdf89995321c951626fac5bcb7c9c49c81ef98
Sophia Franco de Godoy <sophiagodoy.profissional@gmail.com>
2025-05-01 15:52:04 -0300

    Create conteúdo.kt

---
+++ prova 03/conteúdo.kt
@@ -0,0 +1,21 @@
+Avaliação 03 - 12/05 
+
+Avaliação exclusiva de Jetpack Composable 
+
+Como estudar 
+- Ler a documentação principalmente de componentes do Jetpack Composable 
+- Fazer jetpack composable tutorial (material 3)
+- Theming  in Composable (dar uma estudada) 
+- Material Components and Layouts 
+
+Parte pratica 
+- lidar bem com composables, modifiers e layouts (box, column, etc) 
+
+Resolver os desafios: Criar uma tela do limite de cartão do Nubank (Com Customização View)
+- Dominar e fazer a Custom View do artigo do Alexander Jetpack Composable: Custom View (está no Medium) 
+
+- Acrescentar mais dois ou três caras no app de referência de clima (feito pelo professor)  
+
+- na prova vamos ter q escrever o pedaço de uma tela
+- pode pedir para colocar um botão em algum lugar ou um card em algum lugar 
+-
