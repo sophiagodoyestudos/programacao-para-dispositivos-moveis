5d752be1c14d90c7bd37506ef813bf68d4bd823c
Sophia Franco de Godoy <sophiagodoy.profissional@gmail.com>
2025-05-09 08:46:50 -0300

    Update type.kt

--- prova 03/GalaxyWeather/type.kt
+++ prova 03/GalaxyWeather/type.kt
@@ -1,25 +1,24 @@
 // Arquivo para definir a tipografia do meu aplicativo (fontes e estilos de textos)
 
-// Definindo o pacote
 package br.pro.mateus.galaxyweather.ui.theme
 
-// Importação necessárias para definir os estilos de texto
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Criando a tipografia padrão do meu aplicativo
-// A tipografia é armazenada na variável Typography
-// Esse Typography depois é usado no Theme.kt para aplicar os estilos de texto no app inteiro.
+// Estou criando um objeto do tipo Typography e sobrescrevendo apenas o estilo bodyLarge
+/*
+- Estou dizendo ao Compose “para textos de corpo (bodyLarge), use esse tamanho, esse espaçamento e essa família de fonte
+*/
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default, // usa a fonte padrão do sistema.
-        fontWeight = FontWeight.Normal, // peso normal (nem negrito nem fino).
-        fontSize = 16.sp, // tamanho da fonte 16.
-        lineHeight = 24.sp, // altura da linha (espaçamento entre linhas).
-        letterSpacing = 0.5.sp // espaço extra entre as letras.
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
@@ -38,17 +37,3 @@ val Typography = Typography(
     )
     */
 )
-/*
-- Os tipos que estão comentados é um lembrete para você que poderia definir outros estilos, como:
-a) titleLarge: para títulos grandes
-b) labelSmall: para legendas pequenas
-c) bodyLarge: Texto principal do conteúdo, como textos de artigos, descrições
-
-Por que definir a tipografia em um único lugar?
-- Permite que o app tenha consistência visual, ou seja:
-a) Todos os textos principais terão o mesmo tamanho e estilo.
-b) Se quiser mudar o estilo de todos os títulos ou textos de uma vez, é só alterar no Typography.
-- Deixa o app todo padronizado
-
-- Esse arquivo cria o Typography → um conjunto de estilos de texto que define como o texto vai parecer no app inteiro (tamanho, fonte, peso, espaçamento...).
-*/
