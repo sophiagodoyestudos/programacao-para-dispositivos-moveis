042dea4af1646c64b62fdd23dcc24ef254b199cf
Sophia Franco de Godoy <sophiagodoy.profissional@gmail.com>
2025-05-09 08:46:10 -0300

    Update theme.kt

--- prova 03/GalaxyWeather/theme.kt
+++ prova 03/GalaxyWeather/theme.kt
@@ -6,10 +6,8 @@ Esse arquivo define:
 - Isso faz com que o app inteiro fique padronizado e automático → não precisa ficar escolhendo cores ou fontes toda vez que usa Text ou Button, por exemplo
  */
 
-// Definindo o pacote
 package br.pro.mateus.galaxyweather.ui.theme
 
-// Importações para configurar tema, cores, detecção de modo escuro...
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -26,19 +24,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-// Definindo as cores do tema (claro e escuro)
-/*
-Aqui você está definindo:
-a) Quais cores serão usadas no modo escuro → DarkColorScheme.
-b) Quais cores serão usadas no modo claro → LightColorScheme.
-- Essas cores foram definidas lá no seu arquivo Colors.kt
+// Definindo as paletas de cores
 
-As cores principais são:
-- primary (cor principal → botoes, destaques)
-- secondary (cor secundária → complementos)
-- tertiary (opcional → outras ações ou destaques)
-- background (cor do fundo do app)
- */
+// Criando as cores para o tema escuro
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
@@ -47,6 +35,7 @@ private val DarkColorScheme = darkColorScheme(
 
 )
 
+// Criando as cores para o tema claro
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
@@ -64,97 +53,35 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
-// Função do tema do app
-/*
-- Esta é a função que aplica o tema no app inteiro.
-- É ela que você chama no seu setContent da MainActivity
-
-Tudo que você colocar dentro dela vai herdar:
-a) As cores do tema (claro, escuro ou dinâmico)
-b) As fontes do tema (Typography)
- */
 @Composable
 fun GalaxyWeatherTheme(
-    // diz se o app está no modo escuro (automático, detecta pelo sistema com isSystemInDarkTheme()
     darkTheme: Boolean = isSystemInDarkTheme(),
-    /*
-    - dynamicColor é uma opção que permite que o app use as cores do sistema (wallpaper do usuário) caso esteja no Android 12+.
-    a) Se for true, o app tenta usar as cores do sistema.
-    b) Se for false, usa as cores definidas no Colors.kt.
-     */
+    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    // O que vai ser mostrado na tela (os componentes que o usuário criou)
     content: @Composable () -> Unit
 ) {
-    // Definindo qual esquema de cores usar
-    /*
-    - É a lógica que escolhe qual esquema de cores o app vai usar agora
-    a) Vamos usar cores dinâmicas?
-    b) Ou vamos usar o tema escuro?
-    c) Ou vamos usar o tema claro?
-     */
     val colorScheme = when {
-        /*
-        - Se dynamicColor for true E o celular for Android 12+ (S ou superior) → podemos usar as cores dinâmicas do sistema.
-        - (Exemplo: o usuário colocou um papel de parede azul → o sistema sugere cores que combinam com esse azul para o app ficar com a cara do celular).
-         */
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            // Pega o Contexto da tela atual → é necessário para pegar as cores dinâmicas.
             val context = LocalContext.current
-            /*
-            - Verifica se o tema atual é escuro:
-            a) Se for escuro → pega as cores dinâmicas para o tema escuro.
-            b) Se for claro → pega as cores dinâmicas para o tema claro.
-             */
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        // É modo escuro: Usa DarkColorScheme definido no Colors.kt
+
         darkTheme -> DarkColorScheme
-        // É modo claro:	Usa LightColorScheme definido no Colors.kt
         else -> LightColorScheme
     }
 
     val view = LocalView.current
-    /*
-    - Isso verifica se o app está rodando de verdade ou está só no Preview do Android Studio.
-    a) isInEditMode = true → está no Preview → não pode mudar a barra de status, então ignora.
-    b) isInEditMode = false → app real rodando → pode mudar a barra de status.
-     */
     if (!view.isInEditMode) {
-        /*
-        - No Compose, você só pode alterar coisas fora da UI (como status bar) dentro de um SideEffect.
-
-         - SideEffect é um bloco especial no Jetpack Compose usado para executar ações que afetam ou interagem com elementos fora da UI do Compose, como mudar a cor da barra de status, enviar logs ou atualizar algo no sistema.
-         - Ele é necessário porque o Compose reconstrói a interface várias vezes (recomposition), e fazer essas ações diretamente dentro da UI pode causar problemas ou executar múltiplas vezes sem necessidade.
-         - SideEffect garante que essas ações "fora da tela" sejam executadas apenas depois que a composição da interface estiver concluída, de forma segura e controlada.
-         */
         SideEffect {
-            //  Pega a janela da atividade atual (ou seja, da tela que está aberta no app).
             val window = (view.context as Activity).window
-            // Muda a cor da barra de status (lá em cima → relógio, sinal, bateria) para a cor de fundo do tema.
-            window.statusBarColor = colorScheme.background.toArgb()
-            //  Muda a cor da barra de navegação (lá embaixo → botões virtuais ou gestos) para a cor de fundo do tema.
-            window.navigationBarColor = colorScheme.background.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb() // here change the color
+            window.navigationBarColor = colorScheme.background.toArgb() // here change the color
 
-            /*
-            - Define a cor dos ícones da barra de status:
-            a) false → ícones brancos (útil quando o fundo é escuro → ícones precisam ser claros).
-            b) true → ícones pretos (útil quando o fundo é claro → ícones precisam ser escuros)
-             */
+            // here change the status bar element color
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
-    // Aplicando o MaterialTheme
-    /*
-    - Aqui é onde aplica o tema no app inteiro
-
-    a) colorScheme → cores que foram decididas (dinâmicas, light ou dark).
-    b) typography → as fontes definidas no Typography.kt.
-    c) content → os componentes visuais do app.
-
-    - Tudo que você desenhar no app agora vai usar essas cores e fontes.
-     */
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
